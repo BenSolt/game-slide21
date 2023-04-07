058864654ff3e49654d2cfa3811a8bd77304f26c
benjamin-solt <bensolt@gmail.com>
2023-04-07 16:40:30 -0400

    added files and fixed index file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import AppGame2048 from './AppGame2048';
+
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
+    <AppGame2048 />
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 reportWebVitals(sendToVercelAnalytics);
